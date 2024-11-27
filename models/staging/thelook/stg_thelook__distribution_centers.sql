60f5fe0236950064c511f04b12cf9dae137daeb5
Chris Wentworth <chris.wentworth@bytecode.io>
2024-11-27 17:04:04 +0000

    add daily tag for distribution centers

--- models/staging/thelook/stg_thelook__distribution_centers.sql
+++ models/staging/thelook/stg_thelook__distribution_centers.sql
@@ -1,3 +1,5 @@
+{{ config(tags=["daily"]) }}
+
 WITH source AS (
     SELECT {{ dbt_utils.star(from=source('thelook','distribution_centers')) }}
       FROM {{ source('thelook','distribution_centers') }}
